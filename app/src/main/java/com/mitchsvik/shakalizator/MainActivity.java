2c1c0ae4b83f3be3ed54309367db17bb9c2a55f4
Veniamin Zinevych <mitchsvik@gmail.com>
2017-05-29 23:51:39 +0300

    Added shakalled design. Lab2. Also: Moved app folder.

---
+++ app/src/main/java/com/mitchsvik/shakalizator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mitchsvik.shakalizator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
